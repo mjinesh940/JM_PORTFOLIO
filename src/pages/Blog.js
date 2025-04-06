28a6321f5ebe8bb352bd534e092da018e951a888
Jinesh Modi <mjinesh940@gmail.com>
2025-04-06 19:49:07 +0530

    Changes for blogs

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Blog.css';
+import qrImage from '../images/QR_REACT_HOOKS.jpg';
 
 const Blog = () => {
   const blogPosts = [
@@ -14,8 +15,25 @@ const Blog = () => {
       <ul className="blog-list">
         {blogPosts.map((post, index) => (
           <li key={index} className="blog-item">
-            <h2 className="blog-post-title">{post.title}</h2>
-            <p className="blog-post-date">{post.date}</p>
+            {index === 0 ? (
+              <>
+                <a
+                  href="https://jmshines.blogspot.com/2025/04/common-challenges-when-using-react.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="blog-link"
+                >
+                  <h2 className="blog-post-title">{post.title}</h2>
+                </a>
+                <p className="blog-post-date">{post.date}</p>
+                <img src={qrImage} alt="QR Code to blog post" className="qr-code" />
+              </>
+            ) : (
+              <>
+                <h2 className="blog-post-title">{post.title}</h2>
+                <p className="blog-post-date">{post.date}</p>
+              </>
+            )}
           </li>
         ))}
       </ul>
